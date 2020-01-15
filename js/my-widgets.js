d18e1f44c9f8cc61766b5f3c725b1ce40354a8bb
nurlan985 <nurlan985@gmail.com>
2020-01-14 20:39:51 -0600

    docommit

--- js/my-widgets.js
+++ js/my-widgets.js
@@ -1,10 +1,10 @@
 
 var $hMnu = $("#headerMenu");
 if($hMnu != null && $hMnu != undefined) {
-    $hMnu.load("_head-menu.html");
+    $hMnu.load("head-menu.html");
 }
 
 var $footerMnu = $("#footer");
 if($footerMnu != null && $footerMnu != undefined) {
-    $footerMnu.load("_footer.html");
+    $footerMnu.load("footer.html");
 }
