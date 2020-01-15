5332d4e3cb2b864753595e9d4ff94d7f8c97f8f8
nurlan985 <nurlan985@gmail.com>
2020-01-14 20:35:51 -0600

    ass2

---
+++ js/my-widgets.js
@@ -0,0 +1,10 @@
+
+var $hMnu = $("#headerMenu");
+if($hMnu != null && $hMnu != undefined) {
+    $hMnu.load("_head-menu.html");
+}
+
+var $footerMnu = $("#footer");
+if($footerMnu != null && $footerMnu != undefined) {
+    $footerMnu.load("_footer.html");
+}
