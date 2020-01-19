0b639c79fc2a930568337e0a3f858b1bfd8d4e37
nurlan985 <nurlan985@gmail.com>
2020-01-19 17:48:03 -0600

    remake

--- scripts/decoratemytext.js
+++ scripts/decoratemytext.js
@@ -26,14 +26,19 @@ function isVowel(letter){
 }
 function btnIgpay() {
     const $text = $("#text");
-    let t = $text.val();
-    if(t.length>0){
-        if(!isVowel(t[0])){
-            let newText = t.substring(1);
-            $text.val(newText+t[0]);
+    let arr = $text.val().split(' ');
+    let newArr = [];
+    for(let t of arr){
+        let count = t.length;
+        while (!isVowel(t[0]) && count>0){
+            let newT = t.substring(1);
+            t = newT+t[0];
+            count--;
         }
+        t+="-ay";
+        newArr.push(t);
     }
-    $text.val($text.val()+"ay");
+    $text.val(newArr.reduce((e1,e2)=>e1+" "+e2));
 }
 
 function btnMalkovitch() {
